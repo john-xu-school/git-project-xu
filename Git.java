af0080e69817f860da9c9d655cfc1d84ed4cc326
john-xu-school <63273190+john-xu-school@users.noreply.github.com>
2024-09-22 21:46:46 -0700

    Adds GitTester.java; Adds getBlobName, makeBlob, updateIndex, compress in Git.java
    hot dog that's a big commit. This is basically everything in the first assignment

--- Git.java
+++ Git.java
@@ -1,19 +1,23 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.nio.file.*;
 import java.util.Comparator;
 import java.util.stream.Stream;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Git{
-    public static void main (String[] args){
-        Path path = Paths.get("git");
-        resetInit(path);
-        initRepo();
-        initRepo();
+    public boolean isZip = false;
+    public Git(boolean isZip){
+        this.isZip = isZip;
     }
 
-    public static void initRepo(){
-        
+    //initialize repository by creating git folder, objects sub folder, and index file
+    public void initRepo(){
         try{
             Path pathGit = Paths.get("git");
             Path pathObjects = Paths.get("git/objects");
@@ -29,11 +33,11 @@ public class Git{
         }
     }
 
-    private static void resetInit(Path path){
+    //resets the entire git folder path by deleting files recursively
+    public void resetInit(Path path){
         try (Stream<Path> walk = Files.walk(path)) {
             walk.sorted(Comparator.reverseOrder())
             .map(Path::toFile)
-            .peek(System.out::println)
             .forEach(File::delete);
         }
         catch (IOException e){
@@ -41,22 +45,75 @@ public class Git{
         }
     }
 
-    private static String getBlobName(Path path){
+    //gets the name of the blob of the file in variable path by hashing the contents
+    public String getBlobName(Path path){
         try{
-            return "" + Files.readAllBytes(path).hashCode();
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+            byte[] written = Files.readAllBytes(path);
+
+            if (isZip){
+                written = compress(written);
+            }
+
+            byte[] messageDigest = md.digest( written);
+
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            String hashtext = no.toString(16);
+            
+            return hashtext;
 
         } catch (IOException e){
             e.printStackTrace();
             return "";
         }
+        catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+            return "";
+        }
     }
 
-    public static void makeBlob(Path path){
+    //creates the blob of the file in variable path by creating a file with the correct blob name and the same contents as the original
+    public void makeBlob(Path path){
         String name = getBlobName(path);
         try{
-            Files.write(Paths.get("git/"+name), Files.readAllBytes(path));
+            byte[] written = Files.readAllBytes(path);
+            
+            if (isZip){
+                written = compress(written);
+            }
+            Files.write(Paths.get("git/objects/"+name), written);
+            updateIndex(path);
         } catch (IOException e){
             e.printStackTrace();
+        }
+    }
+
+    //updates the index file by adding a new line with entry from blob and file name at variable path 
+    private void updateIndex(Path path){
+        String name = getBlobName(path);
+        String appendString = name + " " + path.getFileName() + "\n";
+        try{
+            Files.write(Paths.get("git/index"), appendString.getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    //zip compresses an array of bytes, usually from files
+    public static byte[] compress(byte[] in) {
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            DeflaterOutputStream defl = new DeflaterOutputStream(out);
+            defl.write(in);
+            defl.flush();
+            defl.close();
+
+            return out.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
---
+++ GitTester.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class GitTester {
+     public static void main (String[] args){
+        Git repo = new Git(true);
+        Path gitPath = Paths.get("git");
+        if (Files.exists(gitPath)) repo.resetInit(gitPath);
+        repo.initRepo();
+        repo.initRepo();
+
+        
+
+        Path fileToBlobPath = Paths.get("README.md");
+        Path fileToBlobPath2 = Paths.get(".gitignore");
+
+        //reseting objects/
+        Path blobPath = Paths.get("git/objects/" + repo.getBlobName(fileToBlobPath));
+        Path blobPath2 = Paths.get("git/objects/" + repo.getBlobName(fileToBlobPath2));
+        if (Files.exists(blobPath )) {
+            try{
+                Files.delete(blobPath);
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        if(Files.exists(blobPath2)){
+            try{
+                Files.delete(blobPath2);
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        //resetting index
+        try{
+            Files.write(Paths.get("git/index"), "".getBytes());
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        
+        //recreating blobs
+        repo.makeBlob(fileToBlobPath);
+        repo.makeBlob(fileToBlobPath2);
+
+        //testing blob name
+        File blob1 = new File(blobPath.toString());
+        File blob2 = new File(blobPath2.toString());
+
+        if (blob1.getName().equals(repo.getBlobName(fileToBlobPath))){
+            System.out.println("Blob 1 name correct");
+        }
+        else{
+            System.out.println("Blob 2 name INCORRECT");
+        }
+
+        if (blob2.getName().equals(repo.getBlobName(fileToBlobPath2))){
+            System.out.println("Blob 2 name correct");
+        }
+        else{
+            System.out.println("Blob 2 name INCORRECT");
+        }
+
+        //Check index content
+        String expectedIndexContents = repo.getBlobName(fileToBlobPath) + " " + fileToBlobPath.getFileName() 
+        + "\n" + repo.getBlobName(fileToBlobPath2) + " " + 
+        fileToBlobPath2.getFileName() + "\n";
+
+        String indexContents = "";
+        try{
+            indexContents = Files.readString(Paths.get("git/index"));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        
+
+        if (expectedIndexContents.equals(indexContents)){
+            System.out.println("Index Updated Correctly");
+        }
+        else{
+            System.out.println("Index Updated INCORRECTLY");
+        }
+
+    }
+}
